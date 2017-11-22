2ba152b057bf79a4d70ef1799e8d6bc7e9df9d42
nhanble <33881576+nhanble@users.noreply.github.com>
2017-11-22 12:58:43 -0600

    Update recycle.js

--- recycle.js
+++ recycle.js
@@ -390,7 +390,7 @@ jQuery(document).ready(function() {
 
     var shows = _.map(showNames, function (name) {
         var show = rel.createShow(name, point.clone());
-        point.y += 46;
+        point.y += 16;
         return show;
     });
     /*
